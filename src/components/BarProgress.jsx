1ef0aef12606413acc06035d2aef05601038f0d2
CrisPer1988 <norberto.cp@hotmail.com>
2023-05-26 14:09:10 -0300

    comentario para Jesus

--- src/components/BarProgress.jsx
+++ src/components/BarProgress.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./styles/barProgress.css"
 
 const BarProgress = ({oneCharacter}) => {
 
     const dataOne = oneCharacter?.map(char => {
-        return char.series.available
-    })
+            return char.series.available
+        })
 
     console.log(dataOne);
     
@@ -19,7 +19,7 @@ const BarProgress = ({oneCharacter}) => {
                 background: `linear-gradient(90deg, #77dad4 0, #b0e3e0 ${dataOne}%, rgb(231, 231, 231) ${dataOne}% 100%)`,
               }}
             >
-
+                BARRA PROGRESIVA
             </div> 
             <div>
                 <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam reprehenderit ab expedita doloremque. Quis quasi soluta voluptas ipsam unde eos non dolorum? Ullam fugit voluptatem repellat sapiente excepturi voluptates praesentium.</h3>
--- src/components/CardCharacter.jsx
+++ src/components/CardCharacter.jsx
@@ -1,74 +1,81 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import "./styles/cardCharacter.css"
-import BarProgress from './BarProgress'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./styles/cardCharacter.css";
+import BarProgress from "./BarProgress";
 
 
-const CardCharacter = ({allCharacters, setAllCharacters}) => {
 
-    const [oneCharacter, setOneCharacter] = useState()
-    const [showModal, setShowModal] = useState(false)
+const CardCharacter = ({ allCharacters, setAllCharacters }) => {
+  const [oneCharacter, setOneCharacter] = useState();
+  const [showModal, setShowModal] = useState(false);
 
-    useEffect(() => {
-        const url = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=1c4aee6a89f573f35cc57ee82e6d38e9&hash=e68f993ec5edf7d29585345c28df9358"
-        axios.get(url)
-        .then(res => setAllCharacters(res.data.data.results))
-        .catch(err => console.log(err))
-      }, [])
+  useEffect(() => {
+    const url =
+      "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=1c4aee6a89f573f35cc57ee82e6d38e9&hash=e68f993ec5edf7d29585345c28df9358";
+    axios
+      .get(url)
+      .then((res) => setAllCharacters(res.data.data.results))
+      .catch((err) => console.log(err));
+  }, []);
 
-      console.log(allCharacters);
-    
-    const handleClick = (id) => {
-        const url = `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=1c4aee6a89f573f35cc57ee82e6d38e9&hash=e68f993ec5edf7d29585345c28df9358`
-        axios.get(url)
-        .then(res => setOneCharacter(res.data.data.results))
-        .catch(err => console.log(err))
-    }
+  console.log(allCharacters);
 
-    console.log(oneCharacter);
+  const handleClick = (id) => {
+    const url = `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=1c4aee6a89f573f35cc57ee82e6d38e9&hash=e68f993ec5edf7d29585345c28df9358`;
+    axios
+      .get(url)
+      .then((res) => setOneCharacter(res.data.data.results))
+      .catch((err) => console.log(err));
+  };
 
-    const show = () => {
-        setShowModal(!showModal)
-    }
-      
+  const show = () => {
+    setShowModal(!showModal);
+  };
+
+  console.log(oneCharacter);
+
   return (
-   <>
-    <BarProgress 
-        oneCharacter={oneCharacter}
-        />
-    <div onClick={show} className='container__cards'>
-       
+    <>
+      <BarProgress oneCharacter={oneCharacter} />
+      
+      <div onClick={show}  className="container__cards">
+
         {
-        allCharacters?.map(character => (
-          <div className='content__card' key={character.id}>
+        allCharacters?.map((character) => (
+          <div className="content__card" key={character.id}>
             <h2>{character.name}</h2>
-            <img onClick={() => handleClick(character.id)} src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt="" /> 
+            <img
+              onClick={() => handleClick(character.id)}
+              src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+              alt=""
+            />
             <h3>Comics: {character.comics.available}</h3>
             <h3>Peliculas: {character.series.available}</h3>
-          </div>
-
-          
-        ))
-        
-      }
-
-      {
-       oneCharacter?.map(char =>(
-        <div className='content__modal'>
-            <div className={`modal ${showModal ? "modal-show" : ""}`}>
-            <h2>{char.name}</h2>
-            <h3>{char.id}</h3>
-            <button onClick={show}>salir</button>
-            </div>
             
-        </div>
-       ))
-      }
-    
+          </div>
+        ))}
 
-    </div>
+
+        
+
+        {/* ESTE ES EL MODAL, AL HABILITARLO FUNCIONA PERO SOLO CAPTURA EL 
+        NOMBRE LA PRIMERA VEZ Y LUEGO LO REPITE, A SU VEZ ANULA LA BARRA PROGRESIVA  */}
+{/* {   
+        oneCharacter?.map((char) => (
+          <div key={char.id} className="content__modal">
+            <div className={`modal ${showModal ? "modal-show" : ""}`}>
+              <h2>{char.name}</h2>
+              <h3>{char.id}</h3>
+              <button onClick={show}>SALIR</button>
+            </div>
+          </div>
+        ))
+        } */}
+</div>
+
+      
     </>
-  )
-}
+  );
+};
 
-export default CardCharacter
+export default CardCharacter;
